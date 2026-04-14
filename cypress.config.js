7e1a5d9147b9802cddc67ed665893217bed4d48e
quality-kels <kelseyjaedoyle@gmail.com>
2026-04-13 17:14:00 -0700

    increased default command timout for CI

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com",
+    defaultCommandTimeout: 10000,
     env: {
       USERNAME: "standard_user",
       PASSWORD: "secret_sauce",
